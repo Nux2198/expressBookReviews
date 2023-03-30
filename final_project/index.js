e6a806c991b3af9af1509c13c64691da1dbefd5a
Nux2198 <Nux2198@github.com>
2023-03-30 01:38:42 -0400

    sending

--- final_project/index.js
+++ final_project/index.js
@@ -11,9 +11,27 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+    if (req.session.authorization) {
+        // get the session token
+        token = req.session.authorization['accessToken'];
+        // verify the token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // cache the user object in the request
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+    });
 //Write the authenication mechanism here
-});
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
